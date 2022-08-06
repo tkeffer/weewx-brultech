47c220bc7a8a6ccb9723fef2f8b77e2ca215c3c2
Tom Keffer <tkeffer@gmail.com>
2022-08-06 16:43:19 -0700

    Update interval as well as delta energy.
    The field interval is set to the number of minutes since the last
    record.
    
    The utility can now be invoked repeatedly. That is, it is idempotent.
    
    Require that the path be on the command line.

--- util/calc_deltas.py
+++ util/calc_deltas.py
@@ -12,53 +12,66 @@ import weewx.manager
 
 import brultech
 
-description = """Convert older-style weewx-brultech databases that stored accumulated energy, to one
-that stores delta energies. 
+description = """Augment older-style weewx-brultech databases that stored only accumulated energy, to one
+that stores delta energies as well. 
 
-The accumulated energies will be left untouched. New columns with delta energies will be added.
+The accumulated energies will be left untouched. New columns with delta energies will be added. 
 
-                 SQLITE ONLY!
+SQLITE ONLY!
 """
 
 usage = """%(prog)s --help
        %(prog)s SQLITE-FILE"""
 
-Need to update interval as well!
-
-SQL_UPDATE_TEMPLATE = """UPDATE archive
-SET %(delta_name)s =
-        (WITH archive_lag AS (SELECT dateTime AS dt,
-                                     %(energy_name)s AS this_val,
-                                     LAG(%(energy_name)s) OVER (ORDER BY dateTime) AS prev_val
-                              FROM archive)
-         SELECT this_val - prev_val
-         FROM archive_lag
-         WHERE archive.dateTime = archive_lag.dt);"""
+SQL_UPDATE_TEMPLATE = """
+WITH archive_lag AS (SELECT dateTime as this_time,
+                            LAG(dateTime) OVER (order by dateTime) as prev_time,
+                            %(energy_name)s as this_val,
+                            LAG(%(energy_name)s) OVER (ORDER BY dateTime) as prev_val
+                     FROM archive)
+UPDATE archive
+SET interval       = (SELECT ifnull((this_time - prev_time) / 60, 5)
+                      FROM archive_lag
+                      WHERE dateTime = archive_lag.this_time),
+    %(delta_name)s = (SELECT (this_val - prev_val) 
+                      FROM archive_lag 
+                      WHERE dateTime = archive_lag.this_time) 
+"""
 
 
 def main():
-    parser = argparse.ArgumentParser(description=description, usage=usage,
+    parser = argparse.ArgumentParser(description=description,
+                                     usage=usage,
                                      prog='calc_deltas')
-    parser.add_argument("sqlite_file", nargs='?', metavar="SQLITE_FILE")
+    parser.add_argument("sqlite_file",
+                        metavar="SQLITE_FILE",
+                        help="Path to the SQLite weewx-brultech database")
     namespace = parser.parse_args()
 
-    if not namespace.sqlite_file:
-        exit("No path to SQLite file. Nothing done.")
+    # It's possible the daily summaries have already been dropped. If so, catch the exception
+    # and switch database managers.
+    try:
+        db_manager = weewx.manager.DaySummaryManager.open({'database_name': namespace.sqlite_file,
+                                                           'driver': 'weedb.sqlite'})
+    except weedb.OperationalError:
+        db_manager = weewx.manager.Manager.open({'database_name': namespace.sqlite_file,
+                                                 'driver': 'weedb.sqlite'})
 
-    db_manager = weewx.manager.DaySummaryManager.open({'database_name': namespace.sqlite_file,
-                                                       'driver': 'weedb.sqlite'})
+    # Find the names of all the accumulated energy fields. These will look like (for example) 'ch5_a_energy2'
+    energy_names = list(filter(lambda key: brultech.energy2_re.match(key), db_manager.obskeys))
 
-    energy_names = sorted(filter(lambda key: brultech.energy2_re.match(key), db_manager.daykeys))
+    # Perform a natural sort before displaying
     energy_names.sort(key=weeutil.weeutil.natural_keys)
-
-    print("Ready to convert columns", *energy_names)
+    print("Ready to augment columns:", energy_names)
 
     ans = weeutil.weeutil.y_or_n("Drop daily summaries and proceed y/n? ")
     if ans == 'n':
         exit("Nothing done")
 
-    db_manager.drop_daily()
-    print("Dropped daily summaries")
+    # Before dropping the daily summaries, check to see if it has already been done
+    if hasattr(db_manager, 'daykeys'):
+        db_manager.drop_daily()
+        print("Dropped daily summaries")
 
     print("Adding new columns")
     t1 = time.time()
@@ -67,16 +80,22 @@ def main():
         delta_name = energy_name.replace("_energy2", "d_energy2")
 
         with weedb.Transaction(db_manager.connection) as cursor:
-            cursor.execute("ALTER TABLE archive ADD COLUMN %s INTEGER" % delta_name)
+            # Try adding the new column. If it already exists, an exception will be raised.
+            # Be prepared to catch it.
+            try:
+                cursor.execute("ALTER TABLE archive ADD COLUMN %s INTEGER" % delta_name)
+            except weedb.OperationalError:
+                pass
             sql_update = SQL_UPDATE_TEMPLATE % {'energy_name': energy_name, 'delta_name': delta_name}
             cursor.execute(sql_update)
 
+        # Give the user a sense of progress:
         print("...added", delta_name, end='\r')
         sys.stdout.flush()
 
     t2 = time.time()
 
-    print("Finished adding new columns in", "%.2f" % (t2-t1), "seconds")
+    print("Finished adding %d new columns in %.2f seconds" % (len(energy_names), t2-t1))
 
 
 if __name__ == "__main__":
